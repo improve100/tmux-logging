9dfadd2f7fb12ea849eeeddb49f1c509ebc856da
939896399@qq.com <improve100>
2025-03-08 15:20:07 +0800

    <fix> fix save complete history single panel as all windows and panes

--- scripts/save_complete_history.sh
+++ scripts/save_complete_history.sh
@@ -7,11 +7,17 @@ source "$CURRENT_DIR/shared.sh"
 
 main() {
 	if supported_tmux_version_ok; then
-		local file=$(expand_tmux_format_path "${save_complete_history_full_filename}")
+		# local file=$(expand_tmux_format_path "${save_complete_history_full_filename}")
+		local timestamp=$(date +"%Y%m%dT%H%M%S")
+		local file=$(expand_tmux_format_path "${save_complete_history_path}/tmux-history-${timestamp}.log")
 		local history_limit="$(tmux display-message -p -F "#{history_limit}")"
-		tmux capture-pane -J -S "-${history_limit}" -p > "${file}"
-		remove_empty_lines_from_end_of_file "${file}"
-		display_message "History saved to ${file}"
+		for _window in $(tmux list-windows -F '#I'); do
+			for _pane in $(tmux list-panes -t ${_window} -F '#P'); do
+				echo "++++++tmux-history-window:${_window}-pane:${_pane}++++++" >> "${file}"
+				tmux capture-pane -J -S "-${history_limit}" -p -t "${_window}.${_pane}" >> "${file}"
+				display_message "History saved to ${file}"
+			done
+		done
 	fi
 }
 main
